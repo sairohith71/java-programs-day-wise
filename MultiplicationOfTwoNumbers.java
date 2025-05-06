ac87a11ce414bcc6dafcd7693e7f85fb2c03f98e
Sai Rohith Royal <149096866+sairohith71@users.noreply.github.com>
2025-05-06 13:35:22 +0530

    Add files via upload

---
+++ MultiplicationOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class MultiplicationOfTwoNumbers {
+public static void main(String[] args) {
+int num1 = 57;
+int num2 = 25;
+int product = num1 * num2;
+System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+}
+}
